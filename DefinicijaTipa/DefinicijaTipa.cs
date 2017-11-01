9b53611924b772806dc89e492a37190b2a62cb25
Julijan Šribar <julijan.sribar@gmail.com>
2017-11-01 19:18:49 +0100

    Add project files.

---
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Vsite.CSharp.DefiniranjeTipa
+{
+    class MojaKlasa
+    {
+        // TODO:010 Dodati privatno polje (podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
+
+
+        // TODO:011 U metodu IspišiČlan dodati naredbu koja će ispisati podatkovni član "broj".
+        public void IspišiČlan()
+        {
+
+        }
+    }
+
+
+    struct MojaStruktura
+    {
+        // TODO:012 Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
+
+
+        // TODO:013 U metodu IspišiČlan dodati naredbu koja će ispisati podatkovni član "tekst".
+        public void IspišiČlan()
+        {
+
+        }
+    }
+
+
+    public class DefinicijaTipa
+    {
+        public static void IspišiČlanKlase()
+        {
+            // TODO:014 Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
+
+        }
+
+        public static void IspišiČlanStrukture()
+        {
+            // TODO:015 Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
+
+        }
+
+        static void Main(string[] args)
+        {
+            IspišiČlanKlase();
+
+            IspišiČlanStrukture();
+
+            Console.WriteLine("GOTOVO!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Internal2/IzvedeneKlase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Vsite.CSharp.DefiniranjeTipa
+{
+    public class IzvedenaIzJavneBazne // : JavnaBazna
+    {
+    }
+
+    public class IzvedenaIzBazne // : Bazna
+    {
+    }
+
+
+
+    internal class InternaBazna
+    {
+        public void IspišiMe()
+        {
+            Console.WriteLine("InternaBazna");
+        }
+    }
+
+    public class IzvedenaIzInterneBazne
+    {
+
+        public void IspišiMe()
+        {
+        }
+    }
+}
